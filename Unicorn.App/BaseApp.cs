d9125948eb2e2e35a65227c0223e14372605985b
Anton Angelov <aangelov@automatetheplanet.com>
2020-11-19 17:54:30 +0200

    add the base app

---
+++ Unicorn.App/BaseApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Unicorn.Container;
+
+namespace Unicorn.App
+{
+    public class BaseApp
+    {
+        public static T Resolve<T>()
+        {
+            T result = ServiceContainer.Resolve<T>();
+            return result;
+        }
+
+        public static void RegisterType<TFrom, TTo>(string name)
+           where TTo : TFrom
+        {
+            ServiceContainer.RegisterType<TFrom, TTo>(name);
+        }
+
+        public static void RegisterType<TFrom, TTo>()
+            where TTo : TFrom
+        {
+            ServiceContainer.RegisterType<TFrom, TTo>();
+        }
+
+        public static void RegisterInstance<TFrom>(TFrom instance)
+        {
+            ServiceContainer.RegisterInstance(instance);
+        }
+    }
+}
